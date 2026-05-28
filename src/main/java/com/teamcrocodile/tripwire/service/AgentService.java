c5deee045c88f8c0841617ebd02af0108a849782
dYalmaz <120399269+dYalmaz@users.noreply.github.com>
2026-05-28 20:30:51 +0100

    AgentService init

--- src/main/java/com/teamcrocodile/tripwire/service/AgentService.java
+++ src/main/java/com/teamcrocodile/tripwire/service/AgentService.java
@@ -1,4 +1,15 @@
 package com.teamcrocodile.tripwire.service;
 
+import com.teamcrocodile.tripwire.model.Agent;
+
+import java.util.List;
+
 public interface AgentService {
+
+
+    Agent addNewAgent(Agent agent);
+    List<Agent> getAllAgents();
+    Agent getAgentById(int id);
+    Agent updateAgentData(int id, Agent agent);
+    void deleteAgent(int id);
 }
--- src/main/java/com/teamcrocodile/tripwire/service/AgentServiceImpl.java
+++ src/main/java/com/teamcrocodile/tripwire/service/AgentServiceImpl.java
@@ -1,4 +1,42 @@
 package com.teamcrocodile.tripwire.service;
 
+import com.teamcrocodile.tripwire.model.Agent;
+
+import java.util.List;
+
 public class AgentServiceImpl implements AgentService {
+
+    private final AgentService agentService;
+
+    public AgentServiceImpl(AgentService agentService) {
+        this.agentService = agentService;
+    }
+
+     @Override
+    public Agent getAgentById(int id) {
+         return agentService.getAgentById(id);
+     }
+
+    @Override
+    public Agent addNewAgent(Agent agent) {
+        return agentService.addNewAgent(agent);
+    }
+
+    @Override
+    public Agent updateAgentData(int id, Agent agent) {
+        return agentService.updateAgentData(id, agent);
+    }
+
+    @Override
+    public void deleteAgent(int id) {
+        agentService.deleteAgent(id);
+    }
+
+    @Override
+    public List<Agent> getAllAgents() {
+        return agentService.getAllAgents();
+    }
+
+
+
 }
