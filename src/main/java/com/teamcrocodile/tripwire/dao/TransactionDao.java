1d621a909e0c8c75f12d8726375bf872ba8c59e6
dYalmaz <120399269+dYalmaz@users.noreply.github.com>
2026-05-28 16:43:40 +0100

    dao init

--- src/main/java/com/teamcrocodile/tripwire/dao/TransactionDao.java
+++ src/main/java/com/teamcrocodile/tripwire/dao/TransactionDao.java
@@ -1,4 +1,14 @@
 package com.teamcrocodile.tripwire.dao;
 
+import com.teamcrocodile.tripwire.model.Transaction;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface TransactionDao {
+        Transaction createTransaction(Transaction transaction);
+        List<Transaction> getAllTransactions();
+        Transaction findTransactionById(int id);
+        void updateTransaction(Transaction transaction);
+        void deleteTransaction(int id);
 }
--- src/main/java/com/teamcrocodile/tripwire/dao/TransactionDaoImpl.java
+++ src/main/java/com/teamcrocodile/tripwire/dao/TransactionDaoImpl.java
@@ -1,4 +1,37 @@
 package com.teamcrocodile.tripwire.dao;
 
-public class TransactionDaoImpl {
+import com.teamcrocodile.tripwire.model.Transaction;
+
+import java.util.List;
+
+public class TransactionDaoImpl implements TransactionDao{
+    @Override
+    public Transaction createTransaction(Transaction transaction) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Transaction findTransactionById(int id) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void updateTransaction(Transaction transaction) {
+        //TODO
+
+    }
+
+    @Override
+    public void deleteTransaction(int id) {
+        //TODO
+
+    }
 }
