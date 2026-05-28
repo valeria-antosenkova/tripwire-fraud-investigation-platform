d1b2c306af6519bb313f75250fde93027e30321e
valeria-antosenkova <sup1qu@gmail.com>
2026-05-27 21:42:13 -0400

    Initialize the app

---
+++ src/test/java/AppTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = App.class)
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
